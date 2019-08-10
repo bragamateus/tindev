856091c5bcb555dba9b6c8440a210d757056042a
Mateus Braga <braga.mateus16@gmail.com>
2019-08-10 00:53:32 -0300

    cadastrando dev no banco de dados

---
+++ backend/src/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+const server = express();
+
+    mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-9e9av.mongodb.net/omnistack8?retryWrites=true&w=majority',
+        {useNewUrlParser: true});
+server.use(express.json());
+server.use(routes);
+
+server.listen(3333);

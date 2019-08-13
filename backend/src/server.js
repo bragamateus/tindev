2fdecfa3e0206f7a4415e1f63315dd55d82bfe77
Mateus Braga <braga.mateus16@gmail.com>
2019-08-13 11:29:39 -0300

    Like e dislike nos perfis de devs e iniciando o frontend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors')
 const routes = require('./routes');
 const server = express();
 
     mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-9e9av.mongodb.net/omnistack8?retryWrites=true&w=majority',
         {useNewUrlParser: true});
+    server.use(cors());
 server.use(express.json());
 server.use(routes);
 
